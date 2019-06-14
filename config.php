a41ee004d0bc7d870db6acf0bab82fcdf04249f3
imamm2 <51767794+imamm2@users.noreply.github.com>
2019-06-14 07:58:05 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['hEX'] = array ('1'=>'hEX!192.168.11.1','hEX@|@imam','hEX#|#nZ2XrpagaWI=','hEX%DiengHotspot','hEX^dh.net','hEX&Rp','hEX*10','hEX(1','hEX)0','hEX=0','hEX@!@enable');
+$data['v3u'] = array ('1'=>'v3u!192.168.11.1','v3u@|@imam','v3u#|#nZ2XrpagaWI=','v3u%DiengHotspot','v3u^dh.net','v3u&Rp','v3u*10','v3u(1','v3u)0','v3u=0','v3u@!@disable');
